d5e48a758b55e9c93988b565aa7e56efa3c90971
jon-jacky <jon@u.washington.edu>
2011-01-16 22:30:56 -0800

    add sockets directory with echo client and server from dropbox

---
+++ sockets/week1_echo_server.py
@@ -0,0 +1,20 @@
+import socket
+import time
+
+host = '' 
+port = 50000
+backlog = 5 
+size = 1024 
+s = socket.socket(socket.AF_INET,
+                  socket.SOCK_STREAM) 
+s.bind((host,port)) 
+s.listen(backlog) 
+while True: 
+    client, address = s.accept()
+    # data = client.recv(size)
+    data = "foobar"
+    if data: 
+        client.send(data)
+        
+    client.close()
+
