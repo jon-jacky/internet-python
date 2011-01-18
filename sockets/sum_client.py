1a066a288458e0bf3c13f0ca71e9e4d73b10b062
jon-jacky <jon@u.washington.edu>
2011-01-17 18:31:54 -0800

    sum_server, _client: working on localhost with cmd line args for host, port numbers; also, both write progress messages to console

--- sockets/sum_client.py
+++ sockets/sum_client.py
@@ -1,13 +1,45 @@
-import socket 
+"""
+Usage:
 
+ python sum_client.py host port x y
+
+for example:
+
+ python localhost 50000 1 2
+
+Omit trailing args to use defaults: localhost 50000 0 0
+Client will exit with exception if args are wrong type or otherwise don't work
+x and y must be strings that convert to floats.
+If this program succeeds, it sends string in valid form for sum_server
+"""
+
+import socket 
+import sys
+
+# defaults, override with command line args if present
 host = 'localhost'
 port = 50000 
+x = 0
+y = 0
+
+nargs = len(sys.argv)
+if nargs > 1:
+    host = sys.argv[1]
+if nargs > 2:
+    port = int(sys.argv[2])
+if nargs > 3:
+    x = float(sys.argv[3]) # ValueError if arg doesn't convert to float
+if nargs > 4:
+    y = float(sys.argv[4])
+
 size = 1024 
 s = socket.socket(socket.AF_INET, 
                   socket.SOCK_STREAM) 
 s.connect((host,port)) 
-s.send('1 2') 
-data = s.recv(size) 
+xy = '%s %s' % (str(x), str(y))
+print 'Send numbers: %s' % xy
+s.send(xy)
+sum = s.recv(size) 
 s.close() 
-print 'Received:', data
+print 'Receive sum :', sum
 
--- sockets/sum_server.py
+++ sockets/sum_server.py
@@ -1,21 +1,49 @@
-import socket
-import time
+"""
+Usage:
 
+ python sum_server.py host port
+
+for example:
+
+ python localhost 50000
+
+Omit trailing args to use defaults: localhost 50000
+Client will exit with exception if args are wrong type or otherwise don't work
+Client must send one string with two floats (as strings) separate by spaces
+otherwise server will exit with exception.  sum_client only sends valid string.
+"""
+
+import socket
+import sys
+
+# defaults, override with command line args if present
 host = 'localhost' 
 port = 50000
+
+nargs = len(sys.argv)
+if nargs > 1:
+    host = sys.argv[1]
+if nargs > 2:
+    port = int(sys.argv[2])
+
 backlog = 5 
 size = 1024 
 s = socket.socket(socket.AF_INET,
                   socket.SOCK_STREAM) 
 s.bind((host,port)) 
 s.listen(backlog) 
+print 'Listening on %s, port %s, with backlog %s, size %s' % \
+    (host,port,backlog,size)
 while True: 
-    client, address = s.accept()
+    client, (local_host, port) = s.accept()
+    print 'Accepting connection from %s, port %s' % (local_host, port)
     data = client.recv(size)
     if data: 
         # assume data is line of text with two floats separated by spaces
+        print 'Received string: %s' % data
         x, y = [ float(n) for n in data.split(' ')]
-        client.send(str(x+y))  
+        sum = str(x+y)
+        print 'Send string    : %s' % sum
+        client.send(sum)
     client.close()
-
 
